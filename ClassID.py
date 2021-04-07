c438822e96a0fc85bf73c2791c66ba9abc033db8
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-04-07 11:20:01 +0200

    Add in ClassId: Constructor, Getters, Setters

--- ClassID.py
+++ ClassID.py
@@ -14,12 +14,69 @@
 #===================================================================================================
 
 from ClassDate import Date
+import datetime
 
 class Id(Date):
     
     #-------- Definition of the constructor ---------
-    def __init__ (self):
-        pass
+    def __init__ (self, register_number, name, first_name, register_day, register_month, register_year):
+        
+        self.registerNumber = register_number
+        self.name = name
+        self.firstName = first_name
+        self.registerDate = Date(register_day, register_month, register_year)
+
+    #-------- Definition of the Getters ---------
+    @property
+    def registerNumber(self):
+        return self.__registerNumber
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def firstName(self):
+        return self.__firstName
+    @property
+    def registerDate(self):
+        return self.__registerDate
+
+    #-------- Definition of the Setters ---------
+    @registerNumber.setter
+    def registerNumber(self, value):
+        #If day is not an integer
+        if isinstance(value, int) == False:
+            raise ValueError
+        #Other case
+        else:
+            self.__registerNumber = value
+
+    @name.setter
+    def name(self, value):
+        #If month is not an integer
+        if isinstance(value, str) == False:
+           raise ValueError
+        #Other case
+        else:
+            self.__name = value
+
+    @firstName.setter
+    def firstName(self, value):
+        #If year is not an integer
+        if isinstance(value, str) == False:
+           raise ValueError
+        #Other case
+        else:
+           self.__firstName = value
+
+    @registerDate.setter
+    def registerDate(self, value):
+        #If error_date is not an integer
+        if isinstance(value, datetime.datetime) == False:
+            raise ValueError
+        #Other case
+        else:
+            self.__registerDate = value
+
 
 
 
