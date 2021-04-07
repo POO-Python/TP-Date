f93d3cd859f06691e7e27e3358d39ed988f01c14
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-04-07 11:01:43 +0200

    Add Class Id

---
+++ ClassID.py
@@ -0,0 +1,26 @@
+#=====================================================================================================================
+#  ?  ABOUT
+#  @author : Clément LO-CASCIO, Thibault BARRAL, Quentin SAR, Jathursan
+#  MEHAVARNAN
+#  @repo : https://github.com/POO-Python/TP-Date
+#  @createdOn : 12/04/2021
+#  @description : TP Dates of the POO module in python, Group N°2.
+#=====================================================================================================================
+
+
+
+#===================================================================================================
+#                                          IMPORT AND START
+#===================================================================================================
+
+from ClassDate import Date
+
+class Id(Date):
+    
+    #-------- Definition of the constructor ---------
+    def __init__ (self):
+        pass
+
+
+
+
